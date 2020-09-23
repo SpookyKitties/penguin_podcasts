b418166cdfcfb70f7bb5f487a6b467c45289f430
Jared Parkinson <jared@parkinson.im>
2020-09-23 13:43:59 -0600

    Parsed Podcast

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,19 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { ElectronService } from "./core/services";
 import { TranslateService } from "@ngx-translate/core";
 import { AppConfig } from "../environments/environment";
 import { db$ } from "../lib/rss/data";
+import { HttpClient } from "@angular/common/http";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor(
     private electronService: ElectronService,
-    private translate: TranslateService
+    private translate: TranslateService,
+    public httpClient: HttpClient
   ) {
     this.translate.setDefaultLang("en");
     console.log("AppConfig", AppConfig);
@@ -27,4 +29,18 @@ export class AppComponent {
 
     db$.init(); //.subscribe();
   }
+  ngOnInit(): void {
+    this.httpClient
+      .get("assets/rss.xml", { responseType: "text" })
+      .subscribe((o) => {
+        const domParser = new DOMParser();
+        console.log(
+          parsePodcast(
+            domParser.parseFromString(o, "text/xml"),
+            "https://skeptoid.com/podcast.xml"
+          )
+        );
+      });
+  }
 }
+import { parsePodcast, Podcast } from "../lib/models/Podcast";
--- src/lib/models/Podcast.ts
+++ src/lib/models/Podcast.ts
@@ -1,4 +1,15 @@
 import { DBItem } from "../PouchRX/DBItem";
+type PodcastImage = {
+  urL?: string;
+  title?: string;
+  link?: string;
+};
+
+type PodcastOwner = {
+  name: string;
+  email?: string;
+};
+
 export class Podcast implements DBItem {
   _id: string;
   _rev?: string;
@@ -11,16 +22,16 @@ export class Podcast implements DBItem {
   subtitle: string;
   author: string;
   description: string;
-  owner: { name: string; email: string };
+  owner?: PodcastOwner;
   explicit: string;
   podcastType: string;
 
-  image: { urL: string; title: string; link: string };
+  image?: PodcastImage;
 }
 
 const rssChannel = "rss > channel";
 
-export function getTextContent(document: Document, selector: string) {
+export function parseTextContent(document: Document, selector: string) {
   const elmTxt = document.querySelector(selector)?.textContent;
 
   return elmTxt ? elmTxt : "";
@@ -35,25 +46,124 @@ function parseTags(document: Document) {
     : ["podcast"];
 }
 
+import seedRandom from "seed-random";
 function parseTitle(document: Document) {
-  return getTextContent(document, `${rssChannel} > title`);
+  return parseTextContent(document, `${rssChannel} > title`);
 }
-function parseID(document: Document) {}
+function parseID(link: string) {
+  return seedRandom(link)().toString().split(".")[1];
+}
 
-export function parsePodcast(document: Document): Podcast {
+function parseLink(document: Document) {
+  return parseTextContent(document, `${rssChannel} > link`);
+}
+
+function parseChildNodeTextContent(
+  document: Document,
+  selector: string,
+  nodeName: string
+) {
+  const elm = queryChildNode(document, selector, nodeName)?.textContent;
+
+  return elm ? elm : "";
+}
+
+function queryChildNode(
+  document: Document,
+  selector: string,
+  nodeName: string
+): HTMLElement | undefined {
+  const elm = document.querySelector(selector);
+  if (elm?.childNodes) {
+    const node = Array.from(elm.childNodes).filter(
+      (n) => n.nodeName === nodeName
+    )[0];
+    console.log(node);
+
+    return node ? (node as HTMLElement) : undefined;
+  }
+  return undefined;
+}
+
+function parseCopyright(document: Document): string {
+  return parseTextContent(document, `${rssChannel} > copyright`);
+}
+function parseAuthor(document: Document): string {
+  return parseChildNodeTextContent(document, `${rssChannel}`, "itunes:author");
+}
+function parseExplicit(document: Document): string {
+  return parseChildNodeTextContent(
+    document,
+    `${rssChannel}`,
+    "itunes:explicit"
+  );
+}
+function parseLanguage(document: Document): string {
+  return parseTextContent(document, `${rssChannel} > language`);
+}
+
+function parseDescription(document: Document) {
+  return parseTextContent(document, `${rssChannel} > description`);
+}
+function parsePodcastType(document: Document) {
+  return parseChildNodeTextContent(document, rssChannel, "itunes:type");
+}
+function parseSubtitle(document: Document) {
+  return parseChildNodeTextContent(document, rssChannel, "itunes:subtitle");
+}
+
+function parseImage(document: Document) {
+  const imageElm = document.querySelector(`${rssChannel} > image`);
+
+  const url = imageElm?.querySelector("url")?.textContent;
+  const title = imageElm?.querySelector("title")?.textContent;
+  const link = imageElm?.querySelector("link")?.textContent;
+
+  return url
+    ? {
+        url: url,
+        title: title ? title : undefined,
+        link: link ? link : undefined,
+      }
+    : undefined;
+}
+// function parseLink(document: Document) {
+// }
+
+function parseOwner(document: Document): PodcastOwner | undefined {
+  const ownerElm = queryChildNode(document, rssChannel, "itunes:owner");
+
+  console.log(ownerElm);
+
+  if (ownerElm) {
+    const nameElm = Array.from(ownerElm.childNodes).filter(
+      (e) => e.nodeName === "itunes:name"
+    )[0]?.textContent;
+    const emailElm = Array.from(ownerElm.childNodes).filter(
+      (e) => e.nodeName === "itunes:email"
+    )[0]?.textContent;
+
+    if (nameElm) {
+      return { email: emailElm ? emailElm : undefined, name: nameElm };
+    }
+  }
+
+  return undefined;
+}
+export function parsePodcast(document: Document, url: string): Podcast {
   return {
-    _id: "",
-    author: "",
-    copyright: "",
-    url: "",
-    description: "",
-    explicit: "",
-    image: { link: "", title: "", urL: "" },
-    language: "",
-    link: "",
-    owner: { email: "", name: "" },
-    podcastType: "",
-    subtitle: "",
+    _id: parseID(url),
+    author: parseAuthor(document),
+    copyright: parseCopyright(document),
+    url: url,
+    description: parseDescription(document),
+    explicit: parseExplicit(document),
+    image: parseImage(document),
+    language: parseLanguage(document),
+    link: parseLink(document),
+    owner: parseOwner(document),
+    podcastType: parsePodcastType(document),
+    subtitle: parseSubtitle(document),
     tags: parseTags(document),
     title: parseTitle(document),
   };
