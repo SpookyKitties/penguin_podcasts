345e1d0d7e29d2b785041031aac010dec6ec53ee
Jared Parkinson <jared@parkinson.im>
2020-10-08 11:01:51 -0600

    adding audio player

--- src/app/components/player/player.component.ts
+++ src/app/components/player/player.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from "@angular/core";
 import { Subscription } from "rxjs";
+import { filter } from "rxjs/operators";
 import { Episode } from "../../../lib/models/parsePodcastEpisodes";
 import { currentEpisode } from "../../../lib/rss/data";
 
@@ -9,16 +10,20 @@ import { currentEpisode } from "../../../lib/rss/data";
   styleUrls: ["./player.component.scss"],
 })
 export class PlayerComponent implements OnInit, OnDestroy {
-  public episode: Subscription;
+  public episode$: Subscription;
+
+  public episode: Episode;
 
   constructor() {}
   ngOnDestroy(): void {
-    this.episode?.unsubscribe();
+    this.episode$?.unsubscribe();
   }
 
   ngOnInit(): void {
-    this.episode = currentEpisode.subscribe((episode) => {
-      console.log(episode);
-    });
+    this.episode$ = currentEpisode
+      .pipe(filter((o) => o !== undefined))
+      .subscribe((episode: Episode) => {
+        this.episode = episode;
+      });
   }
 }
